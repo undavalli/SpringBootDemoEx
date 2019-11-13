c7707472c0be0ad17bd9d1adbf56a2d3c4e27083
raj <raj@rajs-mbp.home>
2019-11-13 10:39:18 -0600

    my second commit

---
+++ src/test/java/com/raj/SpringBatchDemo/SpringBatchDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.raj.SpringBatchDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBatchDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
